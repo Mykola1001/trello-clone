dc4a5cea2c596f9c62f7334dd6b25fe0b1af21b5
Pisker Mykola <macbookpro@Piskers-MacBook-Pro.local>
2018-10-30 22:59:15 +0200

    add task  methods to board service

--- src/app/board/services/board.service.ts
+++ src/app/board/services/board.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { List } from '../models/list';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
   lists: List[];
+  tasks: Task[];
   constructor() { }
 
   addList(list: List): Observable<boolean> {
@@ -19,12 +21,35 @@ export class BoardService {
     return of(true);
   }
 
+  addTask(task: Task): Observable<boolean> {
+    this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    // add generated ID
+    task.id = this.idGenerator();
+    this.tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
+    return of(true);
+  }
+
   editList(list: List): Observable<boolean> {
     this.lists = JSON.parse(localStorage.getItem('lists'));
     for (let i = 0; i < this.lists.length; i++) {
       if (this.lists[i].id === list.id) {
         this.lists[i].name = list.name;
         localStorage.setItem('lists', JSON.stringify(this.lists));
+      }
+    }
+
+    return of(true);
+  }
+
+  editTask(task: Task): Observable<boolean> {
+    this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (this.tasks[i].id === task.id) {
+        this.tasks[i].name = task.name;
+        this.tasks[i].description = task.description;
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
       }
     }
 
@@ -44,8 +69,24 @@ export class BoardService {
     return of(true);
   }
 
+  deleteTask(task: Task): Observable<boolean> {
+    this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (this.tasks[i].id === task.id) {
+        this.tasks.splice(i, 1);
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+      }
+    }
+
+    return of(true);
+  }
+
   getAllLists(): Observable<any> {
     return of(JSON.parse(localStorage.getItem('lists')));
+  }
+
+  getAllTasks(): Observable<any> {
+    return of(JSON.parse(localStorage.getItem('tasks')));
   }
 
   idGenerator(): string {
